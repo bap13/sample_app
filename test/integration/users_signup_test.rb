0f73ce8c5c5d1a338431f7280118e00e495d2c41
Brett Peters <brett.peters1@gmail.com>
2015-10-27 17:29:32 -0600

    add in changes from ch7 exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,4 +23,72 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/show'
   end
+
+  test "blank name should give correct error message" do
+    user = User.new(name: "    ",
+                    email: "valid@example.com",
+                    password: "foobar",
+                    password_confirmation: "foobar")
+    user.save
+    assert_equal user.errors.full_messages[0], "Name can't be blank"
+  end
+
+  test "blank email should give correct error message" do
+    user = User.new(name: "valid",
+                    email: "     ",
+                    password: "foobar",
+                    password_confirmation: "foobar")
+    user.save
+    assert_equal user.errors.full_messages[0], "Email can't be blank"
+  end
+
+  test "invalid email should give correct error message" do
+    user = User.new(name: "valid",
+                    email: "invalid@example",
+                    password: "foobar",
+                    password_confirmation: "foobar")
+    user.save
+    assert_equal user.errors.full_messages[0], "Email is invalid"
+  end
+
+  test "blank password should give correct error message" do
+    user = User.new(name: "valid",
+                    email: "valid@example.com",
+                    password: "",
+                    password_confirmation: "foobar")
+    user.save
+    assert_equal user.errors.full_messages[0], "Password can't be blank"
+  end
+
+  test "short password shoud give correct error message" do
+    user = User.new(name: "valid",
+                    email: "valid@example.com",
+                    password: "foo",
+                    password_confirmation: "foo")
+    user.save
+    assert_equal user.errors.full_messages[0],
+                              "Password is too short (minimum is 6 characters)"
+  end
+
+  test "password/confirmation mismatch shoud give correct error message" do
+    user = User.new(name: "valid",
+                    email: "valid@example.com",
+                    password: "foobar",
+                    password_confirmation: "foobaz")
+    user.save
+    assert_equal user.errors.full_messages[0],
+                              "Password confirmation doesn't match Password"
+  end
+
+  test "valid signup information should result in flash" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "Example User",
+                                            email: "user@example.com",
+                                            password: "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
