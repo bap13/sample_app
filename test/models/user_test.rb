32663a629ad5ad8468984dd08fdc4e61204591be
Brett Peters <brett.peters1@gmail.com>
2015-11-05 19:07:27 -0700

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -71,4 +71,12 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Raw Freegan is the way to go.")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
