2093473e26f7e6306b56eb8faf5756349d04961c
Brett Peters <brett.peters1@gmail.com>
2015-11-06 16:15:36 -0700

    add ch11 exercises

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -4,12 +4,14 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:brett)
+    @other_user = users(:user_1)
   end
 
   test "micropost interface" do
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, micropost: { content: ""}
@@ -17,9 +19,14 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid submission
     content = "This is valid content for a micropost."
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
       post microposts_path, micropost: { content: content }
     end
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, micropost: { content: "Teh Rails logo", picture: picture }
+    end
+    assert assigns(:micropost).picture? 
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -36,5 +43,22 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_no_difference 'Micropost.count' do
       delete micropost_path(microposts(:van))
     end
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+  end
+
+  test "User with zero posts" do
+    log_in_as(@other_user)
+    assert @other_user.microposts.empty?, "user should not have any posts"
+    get root_path
+    assert_template 'static_pages/home'
+    assert_match "0 microposts", response.body
+    @other_user.microposts.create!(content: "bumpus jones best ever.")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 end
